aaa931bf21513b144bcbb627d4ca65d66a4b5220
riosetiawan97 <riosetiawan1997@gmail.com>
2022-07-08 15:55:04 +0700

    08/07/2022
    Menambahkan ViewCategory

---
+++ src/components/admin/category/ViewCategory.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+
+function ViewCategory(){
+
+    const [loading, setLoading] = useState(true);
+    const [categorylist, setCategoryList] = useState([]);
+
+    useEffect(()=>{
+        axios.get('/api/view-category').then(res => {
+            console.log(res.data.category)
+            if(res.status === 200)
+            {
+                setCategoryList(res.data.category)
+            }
+            setLoading(false);
+        });
+    },[]);
+
+    var viewcategory_HTMLTABLE = "";
+    if(loading)
+    {
+        return <h4>Loading...</h4>
+    }
+    else
+    {
+        viewcategory_HTMLTABLE=
+        categorylist.map((item)=>{
+            return (
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.slug}</td>
+                    <td>{item.status}</td>
+                    <td>
+                        <Link to={'edit-category/${item.id}'} className="btn btn-primary btn-sm">Edit</Link>
+                    </td>
+                    <td>
+                        <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                    </td>
+                </tr>
+            )
+        });
+    }
+
+    return (
+        <div className="container px-4">
+            <div className="card mt-4">
+                <div className="card-header">
+                    <h4>Category List
+                        <Link to="/admin/add-category" className="btn btn-primary btn-sm float-end">Add Category</Link>
+                    </h4>
+                </div>                
+                <div className="card-body">
+                    <table className="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Slug</th>
+                                <th>Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {viewcategory_HTMLTABLE}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ViewCategory;
